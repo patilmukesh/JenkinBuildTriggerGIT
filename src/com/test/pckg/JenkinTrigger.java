fa121dd063ea15683eaad2b32737536f017f2fd2
Muks <Muks@Mukesh>
2017-04-05 11:32:07 -0400

    1st Committ

---
+++ src/com/test/pckg/JenkinTrigger.java
@@ -0,0 +1,17 @@
+package com.test.pckg;
+
+import org.testng.annotations.Test;
+
+public class JenkinTrigger {
+
+	public JenkinTrigger()
+	{
+		
+	}
+	@Test
+	public void trigger()
+	{
+		System.out.println("My test method");
+	}
+
+}
